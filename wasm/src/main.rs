8d325b62e74c23ff21fb21147baba629055eb9fa
Rishat Valitov <valitov.rishat@gmail.com>
2021-04-23 12:01:44 +0300

    Wasm integration added

---
+++ wasm/src/main.rs
@@ -0,0 +1,95 @@
+//! Default Compute@Edge template program.
+
+use fastly::http::{header, Method, StatusCode};
+use fastly::{mime, Error, Request, Response};
+use fastly::handle::client_request_and_body;
+
+/// The name of a backend server associated with this service.
+///
+/// This should be changed to match the name of your own backend. See the the `Hosts` section of
+/// the Fastly WASM service UI for more information.
+const NGROK_BACKEND: &str = "Ngrok";
+
+/// The name of a second backend associated with this service.
+const OTHER_BACKEND_NAME: &str = "other_backend_name";
+
+/// The entry point for your application.
+///
+/// This function is triggered when your service receives a client request. It could be used to
+/// route based on the request properties (such as method or path), send the request to a backend,
+/// make completely new requests, and/or generate synthetic responses.
+///
+/// If `main` returns an error, a 500 error response will be delivered to the client.
+#[fastly::main]
+fn main(mut req: Request) -> Result<Response, Error> {
+    // Make any desired changes to the client request.
+    req.set_header(header::HOST, "we-protect-your-privacy.ngrok.io");
+
+    // Filter request methods...
+    match req.get_method() {
+        // Allow GET and HEAD requests.
+        &Method::GET | &Method::HEAD => (),
+
+        // Accept PURGE requests; it does not matter to which backend they are sent.
+        m if m == "PURGE" => return Ok(req.send(NGROK_BACKEND)?),
+
+        // Deny anything else.
+        _ => {
+            return Ok(Response::from_status(StatusCode::METHOD_NOT_ALLOWED)
+                .with_header(header::ALLOW, "GET, HEAD")
+                .with_body_str("This method is not allowed\n"))
+        }
+    };
+
+    // Pattern match on the path.
+    match req.get_path() {
+        "/" => {
+            // Request handling logic could go here...  E.g., send the request to an origin backend
+            // and then cache the response for one minute.
+
+            let script_connect = "<script async src=\"./dist/botd.umd.min.js\" onload=\"getResults()\"></script>";
+            let script_body = "<script>\nasync function getResults() {\nconst botdPromise = FPJSBotDetect.load({\ntoken: \"JzdWIiOiIxMjM0NTY3O\",\nendpoint: \"https://fpjs-botd-dev-use1.fpjs.sh/api/v1\",\nasync: true,\n})\nconst botd = await botdPromise\nconst result = await botd.get({isPlayground: true})\nconsole.log(result)\n}\n</script>";
+
+            req.set_ttl(60);
+            let result = req.send(NGROK_BACKEND).unwrap();
+            let body_str = result.into_body_str();
+            return Ok(Response::from_status(StatusCode::OK)
+                .with_content_type(mime::TEXT_HTML_UTF_8)
+                .with_body(body_str + "FingerprintJS")
+                .with_header("TEST", "test"));
+        }
+        "/img/favicon.ico" => {
+            Ok(req.send(NGROK_BACKEND)?)
+        }
+        "/dist/botd.umd.min.js" => {
+            Ok(req.send(NGROK_BACKEND)?)
+        }
+        // If request is to the `/` path, send a default response.
+        // "/" => Ok(Response::from_status(StatusCode::OK)
+        //     .with_content_type(mime::TEXT_HTML_UTF_8)
+        //     .with_body("<iframe src='https://developer.fastly.com/compute-welcome' style='border:0; position: absolute; top: 0; left: 0; width: 100%; height: 100%'></iframe>\n")),
+
+        // If request is to the `/backend` path, send to a named backend.
+        // "/backend" => {
+        //     // Request handling logic could go here...  E.g., send the request to an origin backend
+        //     // and then cache the response for one minute.
+        //     req.set_ttl(60);
+        //     Ok(req.send(NGROK_BACKEND)?)
+        // }
+
+        // If request is to a path starting with `/other/`...
+        path if path.starts_with("/other/") => {
+            // Send request to a different backend and don't cache response.
+            req.set_pass(true);
+            Ok(req.send(OTHER_BACKEND_NAME)?)
+        }
+
+        // Catch all other requests and return a 404.
+        _ => Ok(Response::from_status(StatusCode::NOT_FOUND)
+            .with_body_str("The page you requested could not be found\n")),
+    }
+
+    // client_request_and_body() {
+    //     req.with_body()
+    // }
+}
