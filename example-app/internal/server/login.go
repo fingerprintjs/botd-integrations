4f5204eee829f8e610400f6944f2e4e941a7c1b3
Roman Vishnevskii <roman.vishnevskii@fingerprintjs.com>
2021-05-04 12:10:36 +0300

    Add logging if bot detected

--- example-app/internal/server/login.go
+++ example-app/internal/server/login.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"github.com/sirupsen/logrus"
 	"net/http"
-	"strconv"
 )
 
 type Description struct {
@@ -26,11 +25,28 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		logrus.Warn("Form parse error: ", err.Error())
 	}
 
-	botProbabilityStr := r.Header.Get("Fpjs-Bot-Prob")
-	botProbabilityFloat, err := strconv.ParseFloat(botProbabilityStr, 64)
+	requestId := r.Header.Get("fpjs-request-id")
+	if requestId != "" {
+		logrus.Info("fpjs-request-id = " + requestId)
+		requestStatus := r.Header.Get("fpjs-request-status")
+		logrus.Info("fpjs-request-status = " + requestStatus)
+		if requestStatus == "ok" {
+			logrus.Info("fpjs-bot-status = " + r.Header.Get("fpjs-bot-status"))
+			logrus.Info("fpjs-bot-prob = " + r.Header.Get("fpjs-bot-prob"))
+			logrus.Info("fpjs-bot-type = " + r.Header.Get("fpjs-bot-type"))
 
-	if err == nil && botProbabilityFloat > 0 {
-		logrus.Warn("Query from bot with probability: ", botProbabilityFloat)
+			logrus.Info("fpjs-search-bot-status = " + r.Header.Get("fpjs-search-bot-status"))
+			logrus.Info("fpjs-search-bot-prob = " + r.Header.Get("fpjs-search-bot-prob"))
+			logrus.Info("fpjs-search-bot-type = " + r.Header.Get("fpjs-search-bot-type"))
+
+			logrus.Info("fpjs-vm-status = " + r.Header.Get("fpjs-vm-status"))
+			logrus.Info("fpjs-vm-prob = " + r.Header.Get("fpjs-vm-prob"))
+			logrus.Info("fpjs-vm-type = " + r.Header.Get("fpjs-vm-type"))
+
+			logrus.Info("fpjs-browser-spoofing-status = " + r.Header.Get("fpjs-browser-spoofing-status"))
+			logrus.Info("fpjs-browser-spoofing-prob = " + r.Header.Get("fpjs-browser-spoofing-prob"))
+		}
+		return
 	}
 
 	login := r.FormValue("login")
