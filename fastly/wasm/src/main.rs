478f014b34d4fe92b13faea05ad97489800c0535
Roman Vishnevskii <roman.vishnevskii@fingerprintjs.com>
2021-05-04 13:37:12 +0300

    Extract token from dictionary

--- fastly/wasm/src/main.rs
+++ fastly/wasm/src/main.rs
@@ -1,7 +1,7 @@
 //! Default Compute@Edge template program.
 
 use fastly::http::{header, Method, StatusCode, HeaderValue};
-use fastly::{mime, Error, Request, Response};
+use fastly::{mime, Error, Request, Response, Dictionary};
 use regex::Regex;
 
 /// The name of a backend server associated with this service.
@@ -14,7 +14,6 @@ const APP_HOST: &str = "botd-example-app.fpjs.sh";
 /// The name of a second backend associated with this service.
 const FPJS_BACKEND: &str = "Botd";
 const FPJS_URL: &str = "https://botd.fpapi.io/api/v1/results";
-const FPJS_TOKEN: &str = "JzdWIiOiIxMjM0NTY3O";
 
 const REQUEST_ID_HEADER: &str = "fpjs-request-id";
 const REQUEST_STATUS_HEADER: &str = "fpjs-request-status";
@@ -34,17 +33,17 @@ const VM_STATUS_HEADER: &str = "fpjs-vm-status";
 const VM_PROB_HEADER: &str = "fpjs-vm-prob";
 const VM_TYPE_HEADER: &str = "fpjs-vm-type";
 
-const IS_BOT_HEADER: &str = "fpjs-is-bot";
-const CHALLENGE_HEADER: &str = "fpjs-challenge-id";
-
-const COOKIE_FPJS_NAME: &str = "botd-request-id=";
+const COOKIE_NAME: &str = "botd-request-id=";
 const COOKIE_HEADER: &str = "cookie";
 const SCRIPT_CONNECT: &str = r#"<script async src="https://unpkg.com/@fpjs-incubator/botd-agent@0/dist/botd.umd.min.js" onload="getResults()"></script>"#;
-const SCRIPT_BODY: &str = r#"
+const SCRIPT_BODY_BEGIN: &str = r#"
     <script>
         async function getResults() {
             const botdPromise = FPJSBotDetect.load({
-            token: "JzdWIiOiIxMjM0NTY3O",
+            token: ""#;
+
+const SCRIPT_BODY_END: &str = r#"
+                    ",
             async: true,
         })
         const botd = await botdPromise
@@ -57,14 +56,18 @@ const FORBIDDEN_BODY: &str = "{\"error\": {\"code\": 403, \"description\": \"For
 const FAILED_STR: &str = "failed";
 const OK_STR: &str = "ok";
 
-fn add_fpjs_script(html: Box<str>) -> String {
+fn add_bot_detection_script(html: Box<str>, token: &str) -> String {
     let mut fpjs_html = String::from(html);
-    let head_close_re = Regex::new(r"(</head.*>)").unwrap();
-    let connect_index = head_close_re.find(&*fpjs_html).unwrap().start();
+    let head_close_regex = Regex::new(r"(</head.*>)").unwrap();
+    let connect_index = head_close_regex.find(&*fpjs_html).unwrap().start();
     fpjs_html.insert_str(connect_index, SCRIPT_CONNECT);
-    let body_open_re = Regex::new(r"(<body.*>)").unwrap();
-    let script_index = body_open_re.find(&*fpjs_html).unwrap().end();
-    fpjs_html.insert_str(script_index, SCRIPT_BODY);
+    let body_open_regex = Regex::new(r"(<body.*>)").unwrap();
+    let mut script_index = body_open_regex.find(&*fpjs_html).unwrap().end();
+    fpjs_html.insert_str(script_index, SCRIPT_BODY_BEGIN);
+    script_index += SCRIPT_BODY_BEGIN.len();
+    fpjs_html.insert_str(script_index, token);
+    script_index += token.len();
+    fpjs_html.insert_str(script_index, SCRIPT_BODY_END);
     return fpjs_html;
 }
 
@@ -160,7 +163,7 @@ fn get_single_result(verify_response: &Response, status_header: String, prob_hea
     return result;
 }
 
-fn bot_detection(req: &Request) -> BotDetectionResult {
+fn bot_detection(req: &Request, token: &str) -> BotDetectionResult {
     let mut result = BotDetectionResult {
         request_id: "".to_owned(),
         request_status: "".to_owned(),
@@ -178,7 +181,7 @@ fn bot_detection(req: &Request) -> BotDetectionResult {
         return result;
     }
     let cookie_value = cookie_option.unwrap();
-    let cookie_element = extract_cookie_element(&*cookie_value, COOKIE_FPJS_NAME);
+    let cookie_element = extract_cookie_element(&*cookie_value, COOKIE_NAME);
     if cookie_element.is_none() {
         result.request_status = FAILED_STR.to_owned();
         return result;
@@ -189,7 +192,7 @@ fn bot_detection(req: &Request) -> BotDetectionResult {
     // Build request for bot detection
     let mut verify_request = Request::get(FPJS_URL);
     let mut query_str: String = "header&token=".to_owned();
-    query_str.push_str(FPJS_TOKEN);
+    query_str.push_str(token);
     query_str.push_str("&id=");
     query_str.push_str(fpjs_request_id.as_str());
     verify_request.set_query_str(query_str);
@@ -240,6 +243,14 @@ fn bot_detection(req: &Request) -> BotDetectionResult {
 /// If `main` returns an error, a 500 error response will be delivered to the client.
 #[fastly::main]
 fn main(mut req: Request) -> Result<Response, Error> {
+    let config = Dictionary::open("config");
+    let token_option = config.get("token");
+    if token_option.is_none() {
+        return Ok(Response::from_status(StatusCode::INTERNAL_SERVER_ERROR)
+            .with_body_str("Token cannot be extracted from fastly configuration\n"))
+    }
+    let token = token_option.unwrap();
+
     // Make any desired changes to the client request.
     req.set_header(header::HOST, APP_HOST);
 
@@ -269,7 +280,7 @@ fn main(mut req: Request) -> Result<Response, Error> {
         "/" => {
             req.set_pass(true); // TODO: get rid of it
             let response = req.send(APP_BACKEND).unwrap();
-            let new_response = add_fpjs_script(Box::from(response.into_body_str()));
+            let new_response = add_bot_detection_script(Box::from(response.into_body_str()), token.as_str());
 
             return Ok(Response::from_status(StatusCode::OK)
                 .with_content_type(mime::TEXT_HTML_UTF_8)
@@ -281,7 +292,7 @@ fn main(mut req: Request) -> Result<Response, Error> {
         }
         "/login" => {
             req.set_pass(true); // TODO: get rid of it
-            let result = bot_detection(&req);
+            let result = bot_detection(&req, token.as_str());
 
             // Decision should we block the request or not
             let botd_calculated = result.request_status.eq(OK_STR)
