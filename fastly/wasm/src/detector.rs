2a2a93b064e1dafec984c02a2368969a131ee371
Rishat Valitov <valitov.rishat@gmail.com>
2021-09-11 01:37:54 +0300

    Add detector

---
+++ fastly/wasm/src/detector.rs
@@ -0,0 +1,65 @@
+use fastly::{Request, Response};
+
+use crate::{ERROR_DESCRIPTION, REQUEST_ID_HEADER, REQUEST_STATUS_HEADER};
+use crate::config::{Config};
+
+pub const PROCESSED: &str = "processed";
+pub const ERROR: &str = "error";
+
+pub const BLOB: String = String::from("");
+
+pub trait Detect {
+    fn make(req: &Request, config: &Config) -> Result<Self, &str>;
+    fn get_request_id(&self) -> String;
+}
+
+pub fn transfer_headers(req: &mut Request, botd_resp: &Response) {
+    static RESULT_HEADERS: [&str; 15] = [
+        "botd-request-id",
+        "botd-request-status",
+        "botd-error-description",
+        "botd-automation-tool-status",
+        "botd-automation-tool-prob",
+        "botd-automation-tool-type",
+        "botd-search-bot-status",
+        "botd-search-bot-prob",
+        "botd-search-bot-type",
+        "botd-browser-spoofing-status",
+        "botd-browser-spoofing-prob",
+        "botd-browser-spoofing-type",
+        "botd-vm-status",
+        "botd-vm-prob",
+        "botd-vm-type"
+    ];
+
+    for header_name in RESULT_HEADERS {
+        if let Some(header_value) = botd_resp.get_header(header_name) {
+            req.set_header(header_name, header_value);
+        };
+    }
+}
+
+pub fn get_request_id(resp: &Response) -> Result<String, &str> {
+    let request_id_header = match resp.get_header(REQUEST_ID_HEADER) {
+        Some(r) => r,
+        None => return Err("[Compute@Edge] Request id cannot be found.")
+    };
+
+    return match request_id_header.to_str() {
+        Ok(s) => Ok(String::from(s)),
+        Err(e) => Err("[Compute@Edge] Can't cast request id header to string.")
+    };
+}
+
+pub fn check_resp(resp: &Response) -> Result<(), &str> {
+    let request_status = match resp.get_header(REQUEST_STATUS_HEADER) {
+        Some(r) => r,
+        None => return Err("[Compute@Edge] Request status cannot be found.")
+    };
+
+    if !request_status.eq(PROCESSED) && resp.get_header(ERROR_DESCRIPTION).is_none() {
+        return Err("[Compute@Edge] Request status is not processed, but error description cannot be found.")
+    }
+
+    Ok(())
+}
