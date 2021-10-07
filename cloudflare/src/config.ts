b25d390fc78ccc86428d73cc5e1045a5908c8eeb
Rishat Valitov <valitov.rishat@gmail.com>
2021-10-07 13:19:48 +0300

    Update Cloudflare integration

--- cloudflare/src/config.ts
+++ cloudflare/src/config.ts
@@ -1,10 +1,13 @@
-import { getPathFromURL, trimURL } from './utils'
-import { BOTD_DEFAULT_URL } from './constants'
+import { getIP, path, trimURL } from './utils'
+import { getRequestIDFromCookie } from './detect'
 
 export default interface Config {
   originURL: string
   botdURL: string
+  realURL: string
   token: string
+  ip: string
+  reqId: string
 }
 
 async function getFromConfig(key: string): Promise<string | null> {
@@ -18,12 +21,15 @@ export async function getConfig(request: Request): Promise<Config> {
   if (token == null) throw Error('Can`t find botd token in KV storage')
 
   let botdURL = await getFromConfig('botd_url')
-  botdURL = botdURL == null ? BOTD_DEFAULT_URL : trimURL(botdURL)
+  botdURL = botdURL == null ? 'https://botd.fpapi.io' : trimURL(botdURL)
 
   let originURL = await getFromConfig('botd_app')
-  originURL = originURL == null ? request.url : trimURL(originURL) + getPathFromURL(request.url)
+  originURL = originURL == null ? request.url : trimURL(originURL) + path(request.url)
 
-  console.log(`[getConfig] Config - Botd URL: ${botdURL}, App URL: ${originURL}, Token: ${token}`)
+  const ip = getIP(request)
+  const reqId = getRequestIDFromCookie(request)
+  const realURL = request.url
+  console.log(`[getConfig] Config - Botd URL: ${botdURL}, App URL: ${originURL}, Token: ${token}, IP: ${ip}, request ID: ${reqId}`)
 
-  return { originURL, botdURL, token }
+  return { originURL, botdURL, realURL, token, ip, reqId }
 }
--- cloudflare/src/index.ts
+++ cloudflare/src/index.ts
@@ -1,12 +1,84 @@
 import { Router } from 'itty-router'
-import handleInitRequest from './handlers/init'
-import handleAll from './handlers/all'
+import Config, { getConfig } from './config'
+import { cloneReqWithURL, isFavicon, isStatic, isInit, isDetect, query } from './utils'
+import { botd, edge, makeCookie, setErrorHeaders } from './detect'
+import { injectScript } from './injector'
+
+async function initHandler(req: Request, config: Config): Promise<Response> {
+  console.log(`[init] Initial request => starting edge bot detection`)
+  await edge(req, config)
+
+  const resp = await fetch(req)
+  console.log(`[init] Origin response finished with status: ${resp.status}, starting code injection...`)
+  const injected = injectScript(await resp.text(), config)
+
+  const respWithScript = new Response(injected, resp)
+  const cookie = makeCookie(req, config)
+  console.log(`[init] Setting cookie ${cookie}`)
+  respWithScript.headers.append('set-cookie', cookie)
+  return respWithScript
+}
+
+async function detectHandler(req: Request, config: Config): Promise<Response> {
+  console.log('[detect] Detect request => redirecting to Botd')
+  const queryString = query(config.realURL)
+  const url = `${config.botdURL}/api/v1/detect${queryString}`
+  const botdReq = cloneReqWithURL(req, url)
+  botdReq.headers.append('botd-client-ip', config.ip)
+  const resp = await fetch(botdReq)
+  const body = await resp.text()
+  console.log(body)
+  const bodyJSON = JSON.parse(body)
+  config.reqId = bodyJSON["requestId"]
+  const cookie = makeCookie(req, config)
+  console.log(`[init] Setting cookie ${cookie}`)
+
+  const mutResp = new Response(body, resp)
+  mutResp.headers.append('set-cookie', cookie)
+
+  return mutResp
+}
+
+async function faviconHandler(req: Request, config: Config): Promise<Response> {
+  console.log('[favicon] Request favicon => starting edge bot detection')
+  await edge(req, config)
+  return await fetch(req)
+}
+
+async function staticHandler(req: Request): Promise<Response> {
+  console.log('[static] Request static data => skipping bot detection')
+  return await fetch(req)
+}
+
+async function nonStaticHandler(req: Request, config: Config): Promise<Response> {
+  await botd(req, config)
+  const resp = await fetch(req)
+  const mutResp = new Response(resp.body, resp)
+  mutResp.headers.append('set-cookie', makeCookie(req, config))
+  return mutResp
+}
+
+async function errorHandler(req: Request, e: Error): Promise<Response> {
+  console.error(`[error] Error handled: ${e.message}`)
+  setErrorHeaders(req, e)
+  return await fetch(req)
+}
+
+async function allHandler(r: Request): Promise<Response> {
+  try {
+    console.log(`[index] Request URL: ${r.url}, Method: ${r.method}`)
+    const config = await getConfig(r)
+    const req = cloneReqWithURL(r, config.originURL)
+
+    if (isInit(req))          return initHandler(req, config)
+    else if (isDetect(req))   return detectHandler(req, config)
+    else if (isFavicon(req))  return faviconHandler(req, config)
+    else if (isStatic(req))   return staticHandler(req)
+    else                      return nonStaticHandler(req, config)
+  } catch (e) {               return errorHandler(r, e) }
+}
 
 const router = Router()
+router.all('*', allHandler)
 
-router.get('/', handleInitRequest)
-router.all('*', handleAll)
-
-addEventListener('fetch', (e) => {
-  e.respondWith(router.handle(e.request))
-})
+addEventListener('fetch', (e) => { e.respondWith(router.handle(e.request)) })
