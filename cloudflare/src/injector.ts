45bef1d2ea18a7c8aba9401a640d3add2b274980
Rishat Valitov <valitov.rishat@gmail.com>
2021-11-02 20:28:39 +0400

    Prepare Cloudflare integration for new agent

--- cloudflare/src/injector.ts
+++ cloudflare/src/injector.ts
@@ -3,16 +3,18 @@ import { insert } from './utils'
 import { PATH_HASH } from './detect'
 
 export function injectScript(html: string, config: Config): string {
+  const scriptSrc = `https://cdn.jsdelivr.net/npm/@fpjs-incubator/botd-agent@${config.version == 'latest' ? "0" : config.version}/dist/botd${config.debug ? ".min" : ""}.js`
+
   const script = `<script>
     function getResults(){
       Botd.load({
         token: "${config.token}",
         endpoint: "${PATH_HASH}",
-        isIntegration: true
+        mode: "integration"
       }).then( b => b.detect() )
     }
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/@fpjs-incubator/botd-agent@0/dist/botd.min.js" onload="getResults()"></script>`
+    <script src="${scriptSrc}" onload="getResults()"></script>`
 
   const match = /(<head.*>)/.exec(html)
   if (match === null) throw Error('Can`t find header tag in request body')
