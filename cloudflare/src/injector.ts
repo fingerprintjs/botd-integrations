5eae1f3ee287d542d15269066de9c571444578ce
Rishat Valitov <valitov.rishat@gmail.com>
2021-11-23 10:53:35 +0300

    Update cdn for Cloudflare

--- cloudflare/src/injector.ts
+++ cloudflare/src/injector.ts
@@ -3,21 +3,23 @@ import { insert } from './utils'
 import { PATH_HASH } from './detect'
 
 export function injectScript(html: string, config: Config): string {
-  const scriptSrc = `https://cdn.jsdelivr.net/npm/@fpjs-incubator/botd-agent@${config.version == 'latest' ? "0" : config.version}/dist/botd${config.debug ? ".min" : ""}.js`
-
-  const script = `<script>
-    function getResults(){
-      Botd.load({
-        token: "${config.token}",
-        endpoint: "${PATH_HASH}",
-        mode: "integration"
-      }).then( b => b.detect() )
-    }
-    </script>
-    <script src="${scriptSrc}" onload="getResults()"></script>`
+  const scriptSrc = `https://openfpcdn.io/botd/v${config.version}/esm${config.debug ? ".min" : ""}.js`
+  const script = `
+    <script>
+      async function getResults() { 
+        import('${scriptSrc}')
+        .then(Botd => Botd.load({
+          token: '${config.token}',
+          endpoint: '${PATH_HASH}',
+          mode: 'integration' 
+        }))
+        .then( detector => detector.detect() ) 
+      }
+      getResults()
+    </script>`
 
   const match = /(<head.*>)/.exec(html)
   if (match === null) throw Error('Can`t find header tag in request body')
 
-  return insert(html, match.index, script)
+  return insert(html, match.index + match[0].length, script)
 }
